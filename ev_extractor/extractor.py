6f281c2e94aa89a2fbf25fc4a4a8d1640dfefa9c
mtriet <minhtriet09@gmail.com>
2019-01-21 17:53:13 +0100

    bug in pipeline fixing

--- ev_extractor/extractor.py
+++ ev_extractor/extractor.py
@@ -5,11 +5,17 @@ from ev_extractor import pipeline
 from util import io
 
 news = io.load_news(embed='none')
-news = news.groupby(news.index)[0].apply(lambda x: [x])
+news = news.groupby(news.index)[0].apply(lambda x: "%s" % '; '.join(x))
+import pandas as pd
+
+news = pd.read_csv('news_copy.csv')
+news = news.set_index(news.pub_date)
+news = news.groupby(news.index)['0'].apply(lambda x: "%s" % ';'.join(x))
 for i, _ in news.iteritems():
     events = []
     print(news.loc[i])
-    for article in news.loc[i]:
-        events.append(pipeline.pipeline(article[0]))
+    all = news.loc[i].split(';')
+    for article in all:
+        events.append(pipeline.pipeline(article))
     news.loc[i] = events
 news.to_csv('event.csv')
