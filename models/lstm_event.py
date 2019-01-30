31ee04863e41d61abc98e860a8b0edec7e1993c4
mtriet <minhtriet09@gmail.com>
2019-01-30 09:42:47 +0100

    add to debug

--- models/lstm_event.py
+++ models/lstm_event.py
@@ -2,20 +2,47 @@ import numpy as np
 from keras import Model
 from keras.initializers import Constant
 from keras.layers import LSTM, Embedding, Input, concatenate
+from keras.preprocessing.sequence import pad_sequences
+
+
+def _padding(event_array: object, max_len) -> object:
+    useless_news = 0
+    # pad events
+    for batch in range(event_array.shape[0]):
+        for day in range(event_array.shape[1]):
+            if type(event_array[batch, day]) == int:
+                useless_news += 1
+                continue
+            for news in event_array[batch, day][:]:
+                if len(news) == 0:
+                    useless_news += 1
+                    event_array[batch, day].remove(news)
+            for news in range(len(event_array[batch, day])):
+                event_array[batch, day][news] = pad_sequences(event_array[batch, day][news], maxlen=max_len, dtype=object)
+            event_array[batch, day] = np.array(event_array[batch, day])
+    print('useless news', useless_news)
+    return event_array
 
 
 def train(x_train, y_train, x_test, y_test, layer_shape, time_steps, epoch, learning_rate, predict_length,
-          embed, vocab_size):
+          embed, max_len):
     x_train_price = x_train[:, :, 0]
     x_train_price = np.expand_dims(x_train_price, axis=2)
     x_train_events = x_train[:, :, 1]
+    x_train_events = _padding(x_train_events, max_len)
+
     x_test_price = x_test[:, :, 0]
     x_test_price = np.expand_dims(x_test_price, axis=2)
     x_test_events = x_test[:, :, 1]
+    x_test_events = _padding(x_test_events, max_len)
+
     seq_length = x_train.shape[1]
     num_words = len(embed) + 1
 
-    price_input = Input(batch_shape=(None, seq_length, 1), dtype=float, name='price_input')
+    # x_train[0,0,:] sentences
+    # x_train[0,0,:][0] sentence
+    # x_train[0,0,:][0][:] event
+    price_input = Input(batch_shape=(None, seq_length, 1), name='price_input')
     event_input = Input(batch_shape=(None, seq_length), name='event_input')
     emb = Embedding(input_dim=num_words, output_dim=300, embeddings_initializer=Constant(embed),
                     mask_zero=True, trainable=False)(event_input)
--- train_event.py
+++ train_event.py
@@ -11,7 +11,6 @@ from util import data_generator, io
 
 nlp = spacy.load('en_core_web_lg')
 
-
 with open('config.yaml') as stream:
     try:
         config = yaml.load(stream)
@@ -42,4 +41,31 @@ x_train, x_test, y_train, y_test = data_generator.generate(window, future=True, 
                                                            )
 lstm_event.train(x_train, y_train, x_test, y_test, time_steps=window, layer_shape=[128, 32],
                  learning_rate=0.0000001, epoch=5000, predict_length=args.predict_length,
-                 embed=embeddings_index, vocab_size=len(vectorizer.get_feature_names()))
+                 embed=embeddings_index, max_len=300)
+# >>> np.percentile(news[0].str.len(), 10)
+# 38.0
+# >>> np.percentile(news[0].str.len(), 20)
+# 45.0
+# >>> np.percentile(news[0].str.len(), 30)
+# 54.0
+# >>> np.percentile(news[0].str.len(), 40)
+# 65.0
+# >>> np.percentile(news[0].str.len(), 50)
+# 79.5
+# >>> np.percentile(news[0].str.len(), 60)
+# 96.0
+# >>> np.percentile(news[0].str.len(), 70)
+# 121.0
+# >>> np.percentile(news[0].str.len(), 80)
+# 153.0
+# >>> np.percentile(news[0].str.len(), 90)
+# 202.0
+# >>> np.percentile(news[0].str.len(), 100)
+# 730.0
+# >>> np.percentile(news[0].str.len(), 95)
+# 251.0
+# >>> np.percentile(news[0].str.len(), 97)
+# 290.0
+# >>> np.percentile(news[0].str.len(), 98)
+# 315.0
+
